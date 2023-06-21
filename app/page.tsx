1cc8e1d4435f91e13bd54a7b86b050739d5e1aca
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-06-22 00:06:04 +0300

    add: header and description text

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,18 @@ import React from 'react'
 
 export default function Home() {
     return (
-        <div>Home</div>
+        <section className='w-full flex-center flex-col '>
+            <h1 className='head_text text-center'>
+                <span>Discover & Share </span>
+                <br />
+                <span
+                    className='orange_gradient'
+                > AI-Powered Prompts</span>
+            </h1>
+            <p className='desc text-center'>
+                Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts
+            </p>
+            {/* Feed */}
+        </section>
     )
 }
