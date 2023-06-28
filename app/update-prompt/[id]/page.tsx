4eef6b013a5ec26bf70679b03a68a2305cf15a36
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-06-28 18:09:11 +0300

    fix: editing problems new tags caused solved

--- app/update-prompt/[id]/page.tsx
+++ app/update-prompt/[id]/page.tsx
@@ -5,15 +5,19 @@ import { useRouter } from 'next/navigation'
 import Form from '@components/Form'
 export default function EditPrompt({ params: { id: promptId } }: { params: { id: string } }) {
     const [submitting, setSubmitting] = useState<boolean>(false)
-    const [post, setPost] = useState<Prompt>({
+    const [post, setPost] = useState<InputPromt>({
         prompt: 'Loading...',
-        tag: 'Loading...',
+        tags: "Loading..."
     })
     useEffect(() => {
         const fetchPost = async (promptId: string) => {
             const response = await fetch(`/api/prompt/${promptId}`)
-            const data: Prompt = await response.json()
-            setPost(data)
+            const data: PromptAndUser = await response.json()
+            const { prompt, tags } = data
+            setPost({
+                prompt,
+                tags: tags.map((tag) => tag.tag).join(' ')
+            })
         }
         if (promptId) fetchPost(promptId)
     }, [promptId])
@@ -21,7 +25,16 @@ export default function EditPrompt({ params: { id: promptId } }: { params: { id:
 
     async function updatePrompt(event: InputEvent) {
         event.preventDefault()
+        const { prompt, tags } = post
+        if (!prompt || !tags) return alert('Please fill out all fields')
 
+        const tagArray = tags.trim().split(' ')
+        if (tagArray.length > 5) return alert('Please only use 5 tags or less')
+        console.log(tagArray);
+        const data = {
+            prompt,
+            tags: tagArray
+        }
         setSubmitting(true);
         if (!promptId) return alert('No prompt id found')
         try {
@@ -30,7 +43,7 @@ export default function EditPrompt({ params: { id: promptId } }: { params: { id:
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify(post)
+                body: JSON.stringify(data)
             })
             if (response.ok) router.push('/profile')
         } catch (error) {
