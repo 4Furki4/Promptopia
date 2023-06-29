c6c52b851ddbedb5b64090956d5ac1e6afb8f606
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-06-29 17:24:41 +0300

    fix: referrer and cache controla added to prompt route

--- app/api/prompt/route.ts
+++ app/api/prompt/route.ts
@@ -1,13 +1,20 @@
 import { connectToDb } from "@utils/database";
 import { NextResponse } from "next/server";
 import Prompt from "@models/prompt";
-
+import { headers } from "next/headers"
 export async function GET() {
+    const headersList = headers();
+    const referer = headersList.get('referer');
     try {
         await connectToDb();
         const promptsAndRelatedUser = await Prompt.find({}).populate("creator");
         return new NextResponse(JSON.stringify(promptsAndRelatedUser), {
             status: 200,
+            // @ts-ignore
+            headers: {
+                referer: referer,
+                'Cache-Control': 'no-cache, no-store, max-age=0, must-revalidate',
+            },
         });
     } catch (err) {
         console.error(err);
@@ -15,4 +22,4 @@ export async function GET() {
             status: 500,
         });
     }
-}
+}   
